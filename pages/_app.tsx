0702b2b1ab57c9fe9e6534201ba3d6b6f3f549dd
hdito <hditow@gmail.com>
2022-11-11 15:08:17 +0300

    Add meta data to page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Портфолио</title>
+        <meta name="description" content="Персональное портфолио" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
