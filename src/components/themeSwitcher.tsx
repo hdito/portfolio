d194dc2ad18b7b3ece4b98693c5f79e1b453a3e6
hdito <hditow@gmail.com>
2025-07-29 23:23:54 +0300

    chore: make theme switcher easier to work with
    - remove magic strings
    - add comment to describe necessary steps for simplifying logic

--- src/components/themeSwitcher.tsx
+++ src/components/themeSwitcher.tsx
@@ -2,13 +2,23 @@ import { ComponentWithProps } from "@/types/componentWithProps";
 import { useEffect, useState } from "react";
 import { IoMoon, IoSunny } from "react-icons/io5";
 
+const THEME_STORAGE_KEY = "theme";
+const DARK_THEME_CLASS = "dark";
+const TOGGLER_TITLE = "Change theme";
+enum THEME {
+  LIGHT = "light",
+  DARK = "dark",
+}
+
 export const ThemeSwitcher: ComponentWithProps = () => {
   const [darkTheme, setDarkTheme] = useState<boolean | null>(null);
 
   useEffect(() => {
+    // This is the easiest way to use browser API in Next.js 13
+    // After switching to App router I'll no longer need it
     if (
-      localStorage?.theme === "dark" ||
-      (!("theme" in localStorage) &&
+      localStorage?.theme === THEME.DARK ||
+      (!(THEME_STORAGE_KEY in localStorage) &&
         window.matchMedia("prefers-color-scheme: dark").matches)
     ) {
       setDarkTheme(true);
@@ -20,27 +30,27 @@ export const ThemeSwitcher: ComponentWithProps = () => {
   useEffect(() => {
     if (darkTheme === null) return;
     if (darkTheme) {
-      setDarkTheme(true);
-      localStorage.theme = "dark";
-      document.documentElement.classList.add("dark");
+      localStorage.theme = THEME.DARK;
+      document.documentElement.classList.add(DARK_THEME_CLASS);
       return;
     }
-    setDarkTheme(false);
-    localStorage.theme = "light";
-    document.documentElement.classList.remove("dark");
+    localStorage.theme = THEME.LIGHT;
+    document.documentElement.classList.remove(DARK_THEME_CLASS);
   }, [darkTheme]);
+
+  const toggleTheme = () => setDarkTheme((prev) => !prev);
 
   return (
     <>
       {darkTheme !== null && (
         <button
           className="absolute -top-4 right-0 flex h-12 items-end justify-center rounded-b-full bg-black p-1 text-2xl text-white dark:bg-gray-50 dark:text-gray-800"
-          onClick={() => setDarkTheme((prev) => !prev)}
+          onClick={toggleTheme}
         >
           {darkTheme ? (
-            <IoMoon title="Change theme" />
+            <IoMoon title={TOGGLER_TITLE} />
           ) : (
-            <IoSunny title="Change theme" />
+            <IoSunny title={TOGGLER_TITLE} />
           )}
         </button>
       )}
