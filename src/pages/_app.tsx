a087db6d9e6f9068c652adaab7b9e3aeba23883e
hdito <hditow@gmail.com>
2025-07-29 23:00:23 +0300

    fix: remove fonts from improper location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import { notoSans } from "@/fonts";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import { notoSans } from "@/components/fonts";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
